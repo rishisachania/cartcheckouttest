336c9c25369508a5b354c30b53d70d36630f8dd4
rishisachania <rishisachania@gmail.com>
2024-08-21 14:10:14 +0100

    failing tests

--- src/test/scala/CheckoutTest.scala
+++ src/test/scala/CheckoutTest.scala
@@ -1,16 +1,18 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class CheckoutTest extends AnyFunSuite {
-  
+
   test("calculateTotal should return 0 for an empty list") {
-    ???
+    assert(CheckoutHelper.calculateTotal(List()) == BigDecimal(0))
   }
 
   test("calculateTotal should return the correct total for a list of apples and oranges") {
-    ???
+    val items = List("Apple", "Apple", "Orange", "Apple")
+    assert(CheckoutHelper.calculateTotal(items) == BigDecimal(2.05))
   }
 
   test("calculateTotal should ignore unrecognized items") {
-    ???
+    val items = List("Apple", "Banana", "Orange", "Apple")
+    assert(CheckoutHelper.calculateTotal(items) == BigDecimal(1.45))
   }
 }
