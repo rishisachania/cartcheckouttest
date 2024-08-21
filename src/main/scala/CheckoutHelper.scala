e66a490b9e12da23e7c7c352a96230eb651433ed
rishisachania <rishisachania@gmail.com>
2024-08-21 14:09:04 +0100

    Adding checkout helper method

--- src/main/scala/CheckoutHelper.scala
+++ src/main/scala/CheckoutHelper.scala
@@ -3,12 +3,6 @@ import Prices._
 
 object CheckoutHelper {
   def calculateTotal(items: List[String]): BigDecimal = {
-    items.foldLeft(BigDecimal(0)) { (total, item) =>
-      item.toLowerCase match {
-        case "apple" => total + applePrice
-        case "orange" => total + orangePrice
-        case _ => total
-      }
-    }
+    0.0
   }
 }
