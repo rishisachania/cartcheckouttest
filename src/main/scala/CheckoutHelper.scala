a96ef90d14339696531380ae809a4016456f5a19
rishisachania <rishisachania@gmail.com>
2024-08-21 14:45:37 +0100

    updating checkout helper for offers

--- src/main/scala/CheckoutHelper.scala
+++ src/main/scala/CheckoutHelper.scala
@@ -3,12 +3,21 @@ import Prices._
 
 object CheckoutHelper {
   def calculateTotal(items: List[String]): BigDecimal = {
-    items.foldLeft(BigDecimal(0)) { (total, item) =>
+    val (apples, oranges) = items.foldLeft((0, 0)) { (counts, item) =>
       item.toLowerCase match {
-        case "apple" => total + applePrice
-        case "orange" => total + orangePrice
-        case _ => total
+        case "apple"  => (counts._1 + 1, counts._2)
+        case "orange" => (counts._1, counts._2 + 1)
+        case _        => counts
       }
     }
+
+    // BOGOF on Apples
+    // 3 for the price of 2 on Oranges
+
+    val appleTotal = (apples / 2 + apples % 2) * applePrice
+    val orangeTotal = ((oranges / 3) * 2 + oranges % 3) * orangePrice
+
+    // apply offers to total
+    appleTotal + orangeTotal
   }
 }
--- src/test/scala/CheckoutTest.scala
+++ src/test/scala/CheckoutTest.scala
@@ -8,11 +8,21 @@ class CheckoutTest extends AnyFunSuite {
 
   test("calculateTotal should return the correct total for a list of apples and oranges") {
     val items = List("Apple", "Apple", "Orange", "Apple")
-    assert(CheckoutHelper.calculateTotal(items) == BigDecimal(2.05))
+    assert(CheckoutHelper.calculateTotal(items) == BigDecimal(1.45))
   }
 
   test("calculateTotal should ignore unrecognized items") {
     val items = List("Apple", "Banana", "Orange", "Apple")
-    assert(CheckoutHelper.calculateTotal(items) == BigDecimal(1.45))
+    assert(CheckoutHelper.calculateTotal(items) == BigDecimal(0.85))
+  }
+
+  test("calculateTotal should apply buy one get one free on apples") {
+    val items = List("Apple", "Apple", "Apple", "Apple")
+    assert(CheckoutHelper.calculateTotal(items) == BigDecimal(1.20)) // 4 apples for 1.20 (2 free)
+  }
+
+  test("calculateTotal should apply 3 for 2 on oranges") {
+    val items = List("Orange", "Orange", "Orange", "Orange", "Orange", "Orange")
+    assert(CheckoutHelper.calculateTotal(items) == BigDecimal(1.00)) // 6 oranges for 1.00 (2 free)
   }
 }
